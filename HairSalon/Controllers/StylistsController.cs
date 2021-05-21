cb1db5fdc39dbd0e4c9f20bb897f39c22f823ea3
Min Chang <minchangmhc@gmail.com>
2021-05-21 14:31:18 -0700

    Add controllers for stylist.

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Salon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly SalonContext _db;
+
+    public StylistsController(SalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("StylistIndex");
+    }
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      thisStylist.Clients = _db.Clients.Where(client => client.StylistId == id).ToList();
+      return View(thisStylist);
+    }
+    public ActionResult Edit(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+    public ActionResult Delete(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(StylistsController => Stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+  }
+}
